e9f0bf65eb6d343bb9fdb57a447fb8335853df90
The M <only.the.em@googlemail.com>
2009-05-13 23:09:07 +0200

    German translation

---
+++ i18n/de-message.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  "Page Metadata" => "Metadaten für Seiten",
+  "Allows to add more metadata to a page." => "Erlaubt es mehrere, unterschiedliche Metadaten zu einer Seite hinzuzufügen.",
+  "More Metadata" => "Weitere Metadaten",
+  "Add Metadata" => "Metadatum hinzufügen",
+  "Add new metadata" => "Neues Metadatum hinzufügen",
+  "This metadata can not be added." => "Dieser Eintrag kann nicht hinzugefügt werden.",
+  "Keyword" => "Schlagwort",
+  "Value" => "Wert",
+  "Delete current metadata?" => "Dieses Metadatum löschen?",
+  "Remove Metadata" => "Metadatum löschen",
+);
+?>
